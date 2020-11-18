81d33e49c8c427cb0788ea55d7e03c52f4854c11
Ren Carothers <rencarothers@gmail.com>
2020-11-17 20:29:15 -0800

    adds user create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,31 +8,53 @@ class UsersController < ApplicationController
     render_404 unless @user
   end
 
-  def login_form
-  end
-
-  def login
-    username = params[:username]
-    if username and user = User.find_by(username: username)
-      session[:user_id] = user.id
+  def create
+    auth_hash = request.env["omniauth.auth"] # how gets info about user's github
+    user = User.find_by(uid: auth_hash[:uid], provider: params[:provider])
+    if user # exists
       flash[:status] = :success
-      flash[:result_text] = "Successfully logged in as existing user #{user.username}"
-    else
-      user = User.new(username: username)
+      flash[:result_text] = "Existing user #{user.username} is logged in."
+    else # user doesn't exist yet
+         # call helper function
+    user = User.build_from_github(auth_hash)
       if user.save
-        session[:user_id] = user.id
         flash[:status] = :success
-        flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
+        flash[:result_text] = "Logged in as new user #{user.username}"
       else
-        flash.now[:status] = :failure
-        flash.now[:result_text] = "Could not log in"
-        flash.now[:messages] = user.errors.messages
-        render "login_form", status: :bad_request
-        return
+        flash[:status] = :failure
+        flash[:result_text] = "Could not create user account #{user.errors.messages}"
+        return redirect_to root_path
       end
     end
+    session[:user_id] = user.id
     redirect_to root_path
   end
+
+  # def login_form
+  # end
+
+  # def login
+  #   username = params[:username]
+  #   if username and user = User.find_by(username: username)
+  #     session[:user_id] = user.id
+  #     flash[:status] = :success
+  #     flash[:result_text] = "Successfully logged in as existing user #{user.username}"
+  #   else
+  #     user = User.new(username: username)
+  #     if user.save
+  #       session[:user_id] = user.id
+  #       flash[:status] = :success
+  #       flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
+  #     else
+  #       flash.now[:status] = :failure
+  #       flash.now[:result_text] = "Could not log in"
+  #       flash.now[:messages] = user.errors.messages
+  #       render "login_form", status: :bad_request
+  #       return
+  #     end
+  #   end
+  #   redirect_to root_path
+  # end
 
   def logout
     session[:user_id] = nil
